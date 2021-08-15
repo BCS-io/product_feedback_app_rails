f832833973ff64e3eb587b9d8a2a4f20f7d688bf
Richard <richard.wigley@gmail.com>
2021-08-15 11:28:39 +0100

    Add notification component

---
+++ app/components/notification_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class NotificationComponent < ViewComponent::Base
+  def initialize(type:, message:)
+    @type = type
+    @message = message
+    @colors = colors(type)
+  end
+
+  def colors(type)
+    case type
+    when "notice"
+      { background: "bg-green-50", text: "text-green-700" }
+    when "alert"
+      { background: "bg-red-50", text: "text-red-700" }
+    else
+      raise UnknownNotificationError, "#{type} notification is unknown"
+    end
+  end
+end
