42360b6cdd1063a24a88957885f22632a0e33e30
Richard <richard.wigley@gmail.com>
2021-08-19 16:54:09 +0100

    User add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, :last_name, presence: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 User.delete_all
 
-(1..5).each do |item|
-  FactoryBot.create(:user, username: "user#{item}")
+path = Rails.root.join("design/starter-code/assets/user-images/")
+images = Dir.children(path)
+
+1.upto(5).each do |index|
+  user = FactoryBot.create(:user, username: "user#{index}")
+  image = images.pop
+  user.avatar.attach(io: File.open(path + image), filename: image, content_type: "image/jpg")
 end
