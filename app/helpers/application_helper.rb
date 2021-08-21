71245ece6596c3eb6d27429fef2b2ff695938d87
Richard <richard.wigley@gmail.com>
2021-08-21 23:10:37 +0100

    Add helper to return error message for any field of any object

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def show_errors(object, field_name)
+    object.errors.messages[field_name].join(", ") if object.errors.any? && object.errors.messages[field_name].present?
+  end
 end
