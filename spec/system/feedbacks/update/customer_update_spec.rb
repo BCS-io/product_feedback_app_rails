c271a04c0319d0a74b76484135fc93b70d2add86
Richard <richard.wigley@gmail.com>
2021-08-22 19:23:50 +0100

    Fet: Customer Feedback#update

---
+++ spec/system/feedbacks/update/customer_update_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+module Feedbacks
+  module Update
+    RSpec.describe "CustomerUpdate", type: :system do
+      it "updates feedback" do
+        customer = create(:customer)
+        create(:feedback, title: "Offer dark version",
+                          status: "suggestion",
+                          user: customer)
+        visit root_path
+        sign_in customer
+
+        click_link "Offer dark version"
+
+        click_link "Edit Feedback"
+
+        fill_in "Title", with: "Different title"
+        select "Enhancement", from: "Category"
+        fill_in "Feedback Detail", with: "I would like to test this"
+
+        click_button "Save Changes"
+
+        expect(page).to have_text "Feedback was successfully updated"
+
+        within ".test-feedback-view-component" do
+          expect(page).to have_text "Different title"
+          expect(page).to have_text "Enhancement"
+          expect(page).to have_text "I would like to test this"
+        end
+      end
+
+      it "displays errors" do
+        customer = create(:customer)
+        create(:feedback, title: "Offer dark version",
+                          status: "suggestion",
+                          user: customer)
+        visit root_path
+        sign_in customer
+
+        click_link "Offer dark version"
+
+        click_link "Edit Feedback"
+
+        fill_in "Title", with: ""
+
+        click_button "Save Changes"
+
+        expect(page).to have_text "can't be blank"
+      end
+    end
+  end
+end
