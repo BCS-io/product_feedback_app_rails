3be23812443edf82dd56ddc224884797e74c5419
Richard <richard.wigley@gmail.com>
2021-08-22 11:14:22 +0100

    Fet: Feedback#create for guest

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root "suggestions#index"
 
+  resources :feedbacks, only: [:new]
   resources :suggestions, only: [:show]
 
   if Rails.env.development?
