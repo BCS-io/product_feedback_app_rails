18e7bf557453b972c5849aa72264c413db281cbe
Richard <richard.wigley@gmail.com>
2021-08-25 14:27:03 +0100

    NewCommentableComponent added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root "suggestions#index"
 
-  resources :feedbacks, only: [:new, :create, :edit, :update, :destroy]
+  resources :feedbacks, only: [:new, :create, :edit, :update, :destroy] do
+    #
+    # module is to make different commentable objects (feedback in this case)
+    # go to different controllers
+    #
+    resources :comments, module: :feedbacks, only: [:create]
+  end
   resources :suggestions, only: [:show]
 
   if Rails.env.development?
