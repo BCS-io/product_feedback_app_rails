0db7f75b56a061b699892af6baef84950d8ae085
Richard <richard.wigley@gmail.com>
2021-08-26 11:56:56 +0100

    Stimulus reply controller for toggling visibility

--- app/components/new_comment_component.rb
+++ app/components/new_comment_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class NewCommentComponent < ViewComponent::Base
-  def initialize(commentable:, parent_id: nil, klass: nil)
+  def initialize(commentable:, parent_id: nil, klass: nil, reply_target: nil)
     @commentable = commentable
     @parent_id = parent_id
     @klass = klass
+    @reply_target = reply_target
   end
 end
