e930b420d1043ee6ab659764567bd4be48c82df4
Richard <richard.wigley@gmail.com>
2021-08-30 13:50:23 +0100

    Tailwindcss-stimulus-components add tab

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,9 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Tabs } from "tailwindcss-stimulus-components"
 
 window.Stimulus = Application.start()
 const context = require.context("controllers", true, /_controller\.(js|ts)$/)
 Stimulus.load(definitionsFromContext(context))
+
