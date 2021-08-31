e3fb6e281e4ff0e8025279774469f4d8b78e8cd9
Richard <richard.wigley@gmail.com>
2021-08-31 23:04:56 +0100

    Add break_point parameter for votes/vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,6 +3,7 @@ class VotesController < ApplicationController
     result = Votes::VotesCreate.new.votes_create(current_user: current_user,
                                                  feedback_id: params[:feedback])
     @vote = result.vote # @vote used in js
+    @break_point = params[:break_point]
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js
@@ -12,6 +13,7 @@ class VotesController < ApplicationController
 
   def destroy
     result = Votes::VotesDestroy.new.votes_destroy(vote_id: params[:id])
+    @break_point = params[:break_point]
     @feedback = result.feedback # @feedback used in js
 
     respond_to do |format|
