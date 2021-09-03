13536efde2301d9c14900ed3eba81734c8da64f6
Richard <richard.wigley@gmail.com>
2021-09-03 13:11:52 +0100

    Fet: FeedbackViewComponent adds text_limit

--- app/components/feedback_view_component.rb
+++ app/components/feedback_view_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class FeedbackViewComponent < ViewComponent::Base
-  def initialize(feedback:, current_user:)
+  def initialize(feedback:, current_user:, text_limit: true)
     @feedback = feedback
     @current_user = current_user
+    @text_limit = text_limit
   end
 end
--- spec/components/feedback_view_component_spec.rb
+++ spec/components/feedback_view_component_spec.rb
@@ -9,6 +9,40 @@ RSpec.describe FeedbackViewComponent, type: :component do
     expect(rendered_component).to have_text("Enhancement")
   end
 
+  describe "#text_limit" do
+    it "applies text limits when true" do
+      feedback = create(:feedback,
+                        title: "this title",
+                        description: "this description",
+                        user: create(:user))
+
+      render_inline(FeedbackViewComponent.new(feedback: feedback,
+                                              current_user: create(:user),
+                                              text_limit: true))
+
+      expect(rendered_component).to have_selector("h3.overflow-hidden.overflow-ellipsis",
+                                                  text: "This title")
+      expect(rendered_component).to have_selector("div.overflow-hidden.overflow-ellipsis",
+                                                  text: "this description")
+    end
+
+    it "has not text limits when false" do
+      feedback = create(:feedback,
+                        title: "this title",
+                        description: "this description",
+                        user: create(:user))
+
+      render_inline(FeedbackViewComponent.new(feedback: feedback,
+                                              current_user: create(:user),
+                                              text_limit: false))
+
+      expect(rendered_component).to have_selector("h3.test-no-text-limit",
+                                                  text: "This title")
+      expect(rendered_component).to have_selector("div.test-no-text-limit",
+                                                  text: "this description")
+    end
+  end
+
   describe "voted" do
     it "has style when not voted" do
       feedback = create(:feedback, title: "another", category: "enhancement", user: create(:user))
