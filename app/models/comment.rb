04260b24afb9709749af3be366a87f1f5129aa0b
Richard <richard.wigley@gmail.com>
2021-09-05 22:04:58 +0100

    Comment adds length, required validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,11 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, counter_cache: true
   belongs_to :parent, optional: true, class_name: "Comment"
 
+  MIN_LENGTH = 3
+  MAX_LENGTH = 250
+  validates :content, presence: true
+  validates :content, length: { minimum: MIN_LENGTH, maximum: MAX_LENGTH }
+
   def comments
     Comment.where(commentable: commentable, parent_id: id)
   end
--- spec/system/feedbacks/show/customer_show_spec.rb
+++ spec/system/feedbacks/show/customer_show_spec.rb
@@ -59,6 +59,37 @@ module Feedbacks
           expect(page).to have_text "second message"
         end
       end
+
+      it "can cause client side validation", js: true do
+        customer = create(:customer, :avatared)
+        feedback = create(:feedback, title: "Visit the show page", user: create(:staff))
+        sign_in customer
+        visit feedback_path(feedback)
+
+        within ".test-new-comment" do
+          fill_in "Add Comment", with: ""
+          click_on "Post Comment"
+
+          message = page.find("#comment_content").native.attribute("validationMessage")
+          #
+          # So selenium_chrome and selenium_chrome headless is giving out two
+          # slightly different messages.
+          #  - Please fill in this field
+          #  - Please fill out this field
+          # Not sure if it's the version of the
+          # browser and if it might change with different versions. Either way
+          # I'm covered for now
+          #
+          expect(message).to have_content /Please fill (out|in) this field/
+        end
+
+        # NOTE: Not got validation for too many characters as HTML 5 validation
+        # is universal (> 99%) and it doesn't show any message it just stops the
+        # user from entering any more.
+        #
+        # If the user bypasses the validation then it will render the
+        # "new comment" again and ask them to fill in but this remains untested.
+      end
     end
   end
 end
