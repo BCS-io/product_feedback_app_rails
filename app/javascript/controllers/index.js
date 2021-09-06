d0882362acb829bb1de026aa5499551034a99c9e
Richard <richard.wigley@gmail.com>
2021-09-06 18:06:32 +0100

    Remove Stimulus-textarea-autogrow
     - there is a bug on initializing the height of a textarea.
       It can shrink the text area down to about 1 line height.
    
       It's flaky so removing it and investigate

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,6 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import { Alert, Slideover, Tabs } from "tailwindcss-stimulus-components"
-import TextareaAutogrow from "stimulus-textarea-autogrow"
 
 window.Stimulus = Application.start()
 const context = require.context("controllers", true, /_controller\.(js|ts)$/)
@@ -13,5 +12,3 @@ Stimulus.load(definitionsFromContext(context))
 window.Stimulus.register("alert", Alert)
 window.Stimulus.register("slideover", Slideover)
 window.Stimulus.register("tabs", Tabs)
-
-window.Stimulus.register("textarea-autogrow", TextareaAutogrow)
