1cab0e7ed5dd08e3c9341b183e400068e1c3dc43
Richard <richard.wigley@gmail.com>
2021-09-09 17:40:15 +0100

    Feedback add length validations
     - there's been nothing suggested. I've put something in large.
     - used postgresql text field which is 65K length limit
     - chosen title of 120 and description of 500 but nothing
       should stop that being larger ... other than how the view
       styling handles it.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,5 +6,9 @@ class Feedback < ApplicationRecord
   enum category: { enhancement: "enhancement", feature: "feature", bug: "bug", ui: "ui", ux: "ux" }, _prefix: true
   enum status: { suggestion: "suggestion", planned: "planned", in_progress: "in_progress", live: "live" }, _prefix: true
 
+  MAX_TITLE_LENGTH = 120
+  MAX_DESCRIPTION_LENGTH = 500
   validates :title, :category, :status, :description, presence: true
+  validates :title, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Feedback, type: :model do
-  %i[title category status description].each do |attribute|
-    it { is_expected.to validate_presence_of(attribute) }
+  describe "validations" do
+    %i[title category status description].each do |attribute|
+      it { is_expected.to validate_presence_of(attribute) }
+    end
+    it { is_expected.to validate_length_of(:title).is_at_most(120) }
+    it { is_expected.to validate_length_of(:description).is_at_most(500) }
   end
 end
