3843883bfb5052f67c2497ca822b58de054cbf05
Richard <richard.wigley@gmail.com>
2021-09-14 10:07:15 +0100

    slideover bar menu.js changes

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -1,15 +1,9 @@
 document.addEventListener("turbolinks:load", function() {
 
-  const btn = document.querySelector(".mobile-menu-button");
-  const sidebar = document.querySelector(".sidebar");
   const hamburger = document.getElementById("hamburger");
-
-  btn.addEventListener("click", () => {
-    sidebar.classList.toggle("translate-x-full");
-  });
+  const navToggle = document.getElementsByClassName("toggle");
 
   hamburger.addEventListener("click", () => {
-    const navToggle = document.getElementsByClassName("toggle");
     for (let i = 0; i < navToggle.length; i++) {
       navToggle.item(i).classList.toggle("hidden");
     }
